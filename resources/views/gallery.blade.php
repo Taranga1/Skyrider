796a7bbdebc3dcc82b8a278f1d1c14b62167d64e
Taranga Baral <tarangabaral1@gmail.com>
2023-04-19 09:21:08 +0545

    schl

--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -18,30 +18,90 @@
     <style>
        
 
-canvas {
-   position: absolute;
-   backface-visibility: hidden;
-   -webkit-backface-visibility: hidden;
-   -moz-backface-visibility: hidden;
-   -ms-backface-visibility: hidden;
+       body {
+  padding: 0px;
+  position: relative;
 }
 
-img {
-    width: 90%;
-   position: absolute;
-   cursor: pointer;
+.gallery {
+  width: 600px;
+  margin: auto;
+  border-radius: 3px;
+  overflow: hidden;
 }
 
-#container {
-   position: absolute;
-   width: 768px;
-   height: 485px;
-   left: 20%;
-   right: 50%;
-   top: 0%;
-   bottom: 0;
-   margin: auto;
+.img-c {
+  width: 200px;
+  height: 200px;
+  float: left;
+  position: relative;
+  overflow: hidden;
 }
+
+.img-w {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background-size: cover;
+  background-position: center;
+  cursor: pointer;
+  transition: transform ease-in-out 300ms;
+}
+
+.img-w img {
+  display: none;
+}
+
+.img-c {
+  transition: width ease 400ms, height ease 350ms, left cubic-bezier(0.4, 0, 0.2, 1) 420ms, top cubic-bezier(0.4, 0, 0.2, 1) 420ms;
+}
+
+.img-c:hover .img-w {
+  transform: scale(1.08);
+  transition: transform cubic-bezier(0.4, 0, 0.2, 1) 450ms;
+}
+
+.img-c.active {
+  width: 100% !important;
+  height: 100% !important;
+  position: absolute;
+  z-index: 2;
+}
+
+.img-c.postactive {
+  position: absolute;
+  z-index: 2;
+  pointer-events: none;
+}
+
+.img-c.active.positioned {
+  left: 0 !important;
+  top: 0 !important;
+  transition-delay: 50ms;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -403,9 +463,22 @@ img {
             
 
 
-            <div id="container"></div>
-            <script src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/175711/delaunay.js"></script>
-            <script src="//cdnjs.cloudflare.com/ajax/libs/gsap/1.13.2/TweenMax.min.js"></script>
+            <br>
+                
+            <div class="gallery">
+                <div class="img-w">
+                  <img src="https://scontent.fbhr1-1.fna.fbcdn.net/v/t31.18172-8/22538988_1507630625985888_9190710994922876043_o.jpg?_nc_cat=106&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=4pGdjgP1qAwAX9oti-n&_nc_ht=scontent.fbhr1-1.fna&oh=00_AfDRtRu1qLVBje5OULVLxFjrKFMpCSiBrl_mTd3FCXmQbg&oe=6466AD60" alt="" /></div>
+                <div class="img-w"><img src="https://imgs.search.brave.com/lWqbDW1WwIMbVH0GrhbeHnsp4D1NzTwkzEaUqfWfzEs/rs:fit:1090:630:1/g:ce/aHR0cHM6Ly93d3cu/c2t5cmlkZXIuZWR1/Lm5wL21lZGlhL2dh/bGxlcnlfaW1hZ2Uv/UHJpbWFyeV9CdWls/ZGluZ19CbG9ja19B/LnBuZw" alt="" /></div>
+                <div class="img-w"><img src="https://imgs.search.brave.com/3W4-sFdYytD523qSBC72BZF848LKQsVN6HcvlJq9Hys/rs:fit:1200:955:1/g:ce/aHR0cHM6Ly93d3cu/c2t5cmlkZXIuZWR1/Lm5wL21lZGlhL3Ns/aWRlcl9pbWcvc2sx/MF8xLmpwZw" alt="" /></div>
+                <div class="img-w"><img src="https://imgs.search.brave.com/czKWh-6Pk9TFo91Znj9LXwY0J_-W-0-20ltCrCZvM4I/rs:fit:1087:432:1/g:ce/aHR0cHM6Ly93d3cu/c2t5cmlkZXIuZWR1/Lm5wL21lZGlhL2dh/bGxlcnlfaW1hZ2Uv/UGx1c18yX0Jsb2Nr/X0FfQi5wbmc" alt="" /></div>
+                <div class="img-w"><img src="https://imgs.search.brave.com/b-2jSSmPGpPxMbdQT45Hw2Hq4TOa719Jp9T6Cb0st1w/rs:fit:1090:712:1/g:ce/aHR0cHM6Ly93d3cu/c2t5cmlkZXIuZWR1/Lm5wL21lZGlhL2dh/bGxlcnlfaW1hZ2Uv/RXh0cmFfQWN0aXZp/dGllc18yLnBuZw" alt="" /></div>
+                <div class="img-w"><img src="https://imgs.search.brave.com/vk-SczzXaQ1rBqO4b4fJqkMs1t1LrKqNnx6pXaflRNc/rs:fit:964:634:1/g:ce/aHR0cHM6Ly93d3cu/c2t5cmlkZXIuZWR1/Lm5wL21lZGlhL2dh/bGxlcnlfaW1hZ2Uv/RXh0cmFfQWN0aXZp/dGllc18zLnBuZw" alt="" /></div>
+                <div class="img-w"><img src="https://imgs.search.brave.com/wU86qsCETACbLVtSx_60wifJ346dXoBFbYnmKdVRWzA/rs:fit:1093:325:1/g:ce/aHR0cHM6Ly93d3cu/c2t5cmlkZXIuZWR1/Lm5wL21lZGlhL2dh/bGxlcnlfaW1hZ2Uv/U2Vjb25kYXJ5X0Js/b2NrX0IucG5n" alt="" /></div>
+                <div class="img-w"><img src="https://imgs.search.brave.com/ogysQ5ROnm1RDBMdAayMwcQWhmxh2ejLOdM9a7WcPYs/rs:fit:690:829:1/g:ce/aHR0cHM6Ly93d3cu/c2t5cmlkZXIuZWR1/Lm5wL21lZGlhL3Vz/ZXJfaW1hZ2VzL3Nr/MTdfR2tMaDZvMi5q/cGc" alt="" /></div>
+                <div class="img-w"><img src="https://imgs.search.brave.com/u46kWaP9qmZ-5muindE6nrSck95fjYjGCIWDvP-2FuE/rs:fit:350:350:1/g:ce/aHR0cHM6Ly9za3ly/aWRlci5lZHUubnAv/c3RhdGljL2ltZy9s/b2cucG5n" alt="" /></div>
+              </div>
+
+            
 
 
 
@@ -618,241 +691,40 @@ img {
     };
 </script>
 
+  
 <script>
-    const TWO_PI = Math.PI * 2;
-
-var images = [],
-    imageIndex = 0;
-
-var image,
-    imageWidth = 768,
-    imageHeight = 485;
-
-var vertices = [],
-    indices = [],
-    fragments = [];
-
-var container = document.getElementById('container');
-
-var clickPosition = [imageWidth * 0.5, imageHeight * 0.5];
-
-window.onload = function() {
-    TweenMax.set(container, {perspective:500});
-
-    // images from reddit/r/wallpapers
-    var urls = [
-            'https://imgs.search.brave.com/vk-SczzXaQ1rBqO4b4fJqkMs1t1LrKqNnx6pXaflRNc/rs:fit:964:634:1/g:ce/aHR0cHM6Ly93d3cu/c2t5cmlkZXIuZWR1/Lm5wL21lZGlhL2dh/bGxlcnlfaW1hZ2Uv/RXh0cmFfQWN0aXZp/dGllc18zLnBuZw',
-            'https://imgs.search.brave.com/ZqwK_AeV7YY9gOV22gMCh50WsJtjAIvEK3fEwrJTsj8/rs:fit:1200:720:1/g:ce/aHR0cHM6Ly9pLnl0/aW1nLmNvbS92aS9G/NmRpSGhWXzI3VS9t/YXhyZXNkZWZhdWx0/LmpwZw',
-            'https://imgs.search.brave.com/3W4-sFdYytD523qSBC72BZF848LKQsVN6HcvlJq9Hys/rs:fit:1200:955:1/g:ce/aHR0cHM6Ly93d3cu/c2t5cmlkZXIuZWR1/Lm5wL21lZGlhL3Ns/aWRlcl9pbWcvc2sx/MF8xLmpwZw',
-            'https://imgs.search.brave.com/lWqbDW1WwIMbVH0GrhbeHnsp4D1NzTwkzEaUqfWfzEs/rs:fit:1090:630:1/g:ce/aHR0cHM6Ly93d3cu/c2t5cmlkZXIuZWR1/Lm5wL21lZGlhL2dh/bGxlcnlfaW1hZ2Uv/UHJpbWFyeV9CdWls/ZGluZ19CbG9ja19B/LnBuZw'
-        ],
-        image,
-        loaded = 0;
-    // very quick and dirty hack to load and display the first image asap
-    images[0] = image = new Image();
-        image.onload = function() {
-            if (++loaded === 1) {
-                imagesLoaded();
-                for (var i = 1; i < 4; i++) {
-                    images[i] = image = new Image();
-
-                    image.src = urls[i];
-                }
-            }
-        };
-        image.src = urls[0];
-};
-
-function imagesLoaded() {
-    placeImage(false);
-    triangulate();
-    shatter();
-}
-
-function placeImage(transitionIn) {
-    image = images[imageIndex];
-
-    if (++imageIndex === images.length) imageIndex = 0;
-
-    image.addEventListener('click', imageClickHandler);
-    container.appendChild(image);
-
-    if (transitionIn !== false) {
-        TweenMax.fromTo(image, 0.75, {y:-1000}, {y:0, ease:Back.easeOut});
-    }
-}
-
-function imageClickHandler(event) {
-    var box = image.getBoundingClientRect(),
-        top = box.top,
-        left = box.left;
-
-    clickPosition[0] = event.clientX - left;
-    clickPosition[1] = event.clientY - top;
-
-    triangulate();
-    shatter();
-}
-
-function triangulate() {
-    var rings = [
-            {r:50, c:12},
-            {r:150, c:12},
-            {r:300, c:12},
-            {r:1200, c:12} // very large in case of corner clicks
-        ],
-        x,
-        y,
-        centerX = clickPosition[0],
-        centerY = clickPosition[1];
-
-    vertices.push([centerX, centerY]);
-
-    rings.forEach(function(ring) {
-        var radius = ring.r,
-            count = ring.c,
-            variance = radius * 0.25;
-
-        for (var i = 0; i < count; i++) {
-            x = Math.cos((i / count) * TWO_PI) * radius + centerX + randomRange(-variance, variance);
-            y = Math.sin((i / count) * TWO_PI) * radius + centerY + randomRange(-variance, variance);
-            vertices.push([x, y]);
-        }
+$(function () {
+    $(".img-w").each(function () {
+      $(this).wrap("<div class='img-c'></div>");
+      let imgSrc = $(this).find("img").attr("src");
+      $(this).css("background-image", "url(" + imgSrc + ")");
     });
-
-    vertices.forEach(function(v) {
-        v[0] = clamp(v[0], 0, imageWidth);
-        v[1] = clamp(v[1], 0, imageHeight);
+  
+    $(".img-c").click(function () {
+      let w = $(this).outerWidth();
+      let h = $(this).outerHeight();
+      let x = $(this).offset().left;
+      let y = $(this).offset().top;
+  
+      $(".active").not($(this)).remove();
+      let copy = $(this).clone();
+      copy.insertAfter($(this)).height(h).width(w).delay(500).addClass("active");
+      $(".active").css("top", y - 8);
+      $(".active").css("left", x - 8);
+  
+      setTimeout(function () {
+        copy.addClass("positioned");
+      }, 0);
     });
-
-    indices = Delaunay.triangulate(vertices);
-}
-
-function shatter() {
-    var p0, p1, p2,
-        fragment;
-
-    var tl0 = new TimelineMax({onComplete:shatterCompleteHandler});
-
-    for (var i = 0; i < indices.length; i += 3) {
-        p0 = vertices[indices[i + 0]];
-        p1 = vertices[indices[i + 1]];
-        p2 = vertices[indices[i + 2]];
-
-        fragment = new Fragment(p0, p1, p2);
-
-        var dx = fragment.centroid[0] - clickPosition[0],
-            dy = fragment.centroid[1] - clickPosition[1],
-            d = Math.sqrt(dx * dx + dy * dy),
-            rx = 30 * sign(dy),
-            ry = 90 * -sign(dx),
-            delay = d * 0.003 * randomRange(0.9, 1.1);
-        fragment.canvas.style.zIndex = Math.floor(d).toString();
-
-        var tl1 = new TimelineMax();
-
-
-        tl1.to(fragment.canvas, 1, {
-            z:-500,
-            rotationX:rx,
-            rotationY:ry,
-            ease:Cubic.easeIn
-        });
-        tl1.to(fragment.canvas, 0.4,{alpha:0}, 0.6);
-
-        tl0.insert(tl1, delay);
-
-        fragments.push(fragment);
-        container.appendChild(fragment.canvas);
-    }
-
-    container.removeChild(image);
-    image.removeEventListener('click', imageClickHandler);
-}
-
-function shatterCompleteHandler() {
-    // add pooling?
-    fragments.forEach(function(f) {
-        container.removeChild(f.canvas);
-    });
-    fragments.length = 0;
-    vertices.length = 0;
-    indices.length = 0;
-
-    placeImage();
-}
-
-//////////////
-// MATH UTILS
-//////////////
-
-function randomRange(min, max) {
-    return min + (max - min) * Math.random();
-}
-
-function clamp(x, min, max) {
-    return x < min ? min : (x > max ? max : x);
-}
-
-function sign(x) {
-    return x < 0 ? -1 : 1;
-}
-
-//////////////
-// FRAGMENT
-//////////////
-
-Fragment = function(v0, v1, v2) {
-    this.v0 = v0;
-    this.v1 = v1;
-    this.v2 = v2;
-
-    this.computeBoundingBox();
-    this.computeCentroid();
-    this.createCanvas();
-    this.clip();
-};
-Fragment.prototype = {
-    computeBoundingBox:function() {
-        var xMin = Math.min(this.v0[0], this.v1[0], this.v2[0]),
-            xMax = Math.max(this.v0[0], this.v1[0], this.v2[0]),
-            yMin = Math.min(this.v0[1], this.v1[1], this.v2[1]),
-            yMax = Math.max(this.v0[1], this.v1[1], this.v2[1]);
-
-        this.box ={
-            x:xMin,
-            y:yMin,
-            w:xMax - xMin,
-            h:yMax - yMin
-        };
-    },
-    computeCentroid:function() {
-        var x = (this.v0[0] + this.v1[0] + this.v2[0]) / 3,
-            y = (this.v0[1] + this.v1[1] + this.v2[1]) / 3;
-
-        this.centroid = [x, y];
-    },
-    createCanvas:function() {
-        this.canvas = document.createElement('canvas');
-        this.canvas.width = this.box.w;
-        this.canvas.height = this.box.h;
-        this.canvas.style.width = this.box.w + 'px';
-        this.canvas.style.height = this.box.h + 'px';
-        this.canvas.style.left = this.box.x + 'px';
-        this.canvas.style.top = this.box.y + 'px';
-        this.ctx = this.canvas.getContext('2d');
-    },
-    clip:function() {
-        this.ctx.translate(-this.box.x, -this.box.y);
-        this.ctx.beginPath();
-        this.ctx.moveTo(this.v0[0], this.v0[1]);
-        this.ctx.lineTo(this.v1[0], this.v1[1]);
-        this.ctx.lineTo(this.v2[0], this.v2[1]);
-        this.ctx.closePath();
-        this.ctx.clip();
-        this.ctx.drawImage(image, 0, 0);
-    }
-};
+  });
+  
+  $(document).on("click", ".img-c.active", function () {
+    let copy = $(this);
+    copy.removeClass("positioned active").addClass("postactive");
+    setTimeout(function () {
+      copy.remove();
+    }, 500);
+  });
 </script>
 
 </html>
